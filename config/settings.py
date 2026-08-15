7e7e21aef6f8a5dc4496b62309a29964fb77f8b7
arif-kdt <arifrahmathmanzil@gmail.com>
2026-08-16 02:43:58 +0530

    Configure allowed hosts for Seenode

--- config/settings.py
+++ config/settings.py
@@ -33,10 +33,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS',
+    'localhost,127.0.0.1'
+).split(',')
 
 
 # =========================================================
